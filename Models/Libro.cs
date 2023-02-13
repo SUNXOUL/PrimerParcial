dd13422033e40b3c8b4d7d00e5aeb496e5d10c0e
Kevin Duran Bruno <123a2017kdb@gmail.com>
2023-02-13 18:17:34 -0400

    Paso 2

---
+++ Models/Libro.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+class Libro
+{
+    [Key]
+    public int LibroId {get;set;}
+
+    public string Titulo{get;set;}
+    public double Precio {get;set;}
+}
