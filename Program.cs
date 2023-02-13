6dc5cf9c78542dcae932cbb77985dd96c2b52cff
Kevin Duran Bruno <123a2017kdb@gmail.com>
2023-02-13 18:50:48 -0400

    3er Commit - Migraciones y BD

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using _1er_Parcial.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Contexto>(Options=>Options.UseSqlite(ConStr));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
